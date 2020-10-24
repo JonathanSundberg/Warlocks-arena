dba27cd190d3524cb53b7cbec9a7596ae9c996c6
Jonathan Sundberg <guitaraddict95@hotmail.com>
2020-10-24 16:24:07 +0200

    Starting on spawning script

---
+++ warlocks-arena/Assets/Scripts/Systems/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+public class PlayerSpawner : ComponentSystem
+{
+    private float spawnTimer;
+    private Random random;
+   
+
+    protected override void OnCreate()
+    {
+        random = new Random(10);
+    }
+    protected override void OnUpdate()
+    {
+        spawnTimer -= Time.DeltaTime;
+        if(spawnTimer <= 0)
+        {
+            spawnTimer = .5f;
+            Entities.ForEach((ref PlayerComponent player) =>
+            {
+                Entity spawnedEntity = EntityManager.Instantiate(player.Prefab);
+                EntityManager.SetComponentData(spawnedEntity, new Translation { Value = player.spawnPos.Value });
+            });
+        }
+    }
+}
